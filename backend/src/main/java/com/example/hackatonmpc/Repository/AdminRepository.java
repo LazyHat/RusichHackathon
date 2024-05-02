71324f62ec8e5de5127550dd01d1719d75b73c7d
Alex <makargravanov@gmail.com>
2024-05-02 18:23:39 +0300

    AAAA

--- backend/src/main/java/com/example/hackatonmpc/Repository/AdminRepository.java
+++ backend/src/main/java/com/example/hackatonmpc/Repository/AdminRepository.java
@@ -4,7 +4,10 @@ import com.example.hackatonmpc.Entity.AdminEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<AdminEntity, Long> {
 
+    Optional<AdminEntity> findByLogin(String login);
 }
