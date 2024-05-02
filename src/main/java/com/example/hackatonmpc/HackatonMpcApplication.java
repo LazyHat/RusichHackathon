dc5c75be1a5146d688702c6236f7c40e6ec0e12f
Alex <makargravanov@gmail.com>
2024-05-02 11:07:08 +0300

    AAAA

---
+++ src/main/java/com/example/hackatonmpc/HackatonMpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackatonmpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonMpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonMpcApplication.class, args);
+    }
+
+}
