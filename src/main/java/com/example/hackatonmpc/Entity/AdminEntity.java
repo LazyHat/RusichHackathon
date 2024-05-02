760494229ae7ff8f3e555db73c46bbb824b6ed5f
Alex <makargravanov@gmail.com>
2024-05-02 11:55:49 +0300

    AAAA

--- src/main/java/com/example/hackatonmpc/Entity/AdminEntity.java
+++ src/main/java/com/example/hackatonmpc/Entity/AdminEntity.java
@@ -1,7 +1,24 @@
 package com.example.hackatonmpc.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class AdminEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String login;
+    private String password;
 }
